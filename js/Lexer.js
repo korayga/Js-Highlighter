3a6a24a44ea957a1f99e2503ee66617863c4e66f
koray garip <156468787+korayga@users.noreply.github.com>
2025-06-03 15:43:55 +0300

    Update Lexer.js

--- js/Lexer.js
+++ js/Lexer.js
@@ -6,7 +6,7 @@ class Lexer {
             {type: 'COMMENT', regex: /^\/\/.*/, ignore: false},
             {type: 'COMMENT', regex: /^\/\*[\s\S]*?\*\//, ignore: false},
 
-            // Anahtar Kelimeler (Rezerve Edilmiş Sözcükler) - Gelişmiş liste 
+            // Anahtar Kelimeler
             {
                 type: 'KEYWORD',
                 regex: /^\b(abstract|arguments|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield|async|of)\b/
